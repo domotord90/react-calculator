00c957d5acd4d411d98c7028dda94b817397483b
domotord90 <jazzmaster@freemail.com>
2019-11-16 16:44:15 +0100

    Updates

---
+++ precache-manifest.d678aed004f2ceccadaddd4b9e4ecf03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43e328c24fc5a66088b6efeb62fe1cc",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "476d1cf3b936272b2e59",
+    "url": "/react-calculator/static/css/main.1e2ef51c.chunk.css"
+  },
+  {
+    "revision": "b421cd4fb9c397abade6",
+    "url": "/react-calculator/static/js/2.f133e636.chunk.js"
+  },
+  {
+    "revision": "476d1cf3b936272b2e59",
+    "url": "/react-calculator/static/js/main.9663dbf3.chunk.js"
+  },
+  {
+    "revision": "2c7460d17796b892bd56",
+    "url": "/react-calculator/static/js/runtime~main.a948531e.js"
+  },
+  {
+    "revision": "ee9072cfc9314d18a8e614104a805ab1",
+    "url": "/react-calculator/static/media/DIGITALDREAM.ee9072cf.ttf"
+  }
+]);
